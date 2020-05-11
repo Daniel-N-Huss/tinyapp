19b2c84cb7aee43336c093004e877ca7594fc6fb
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-11 22:59:52 +0000

    created a url index page and started routing to it

--- express_server.js
+++ express_server.js
@@ -18,18 +18,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
